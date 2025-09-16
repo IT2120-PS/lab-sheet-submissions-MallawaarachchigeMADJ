bee9d2f98d9416bcd3c5052a20b32e0faeac16c3
MallawaarachchigeMADJ <it24102172@my.sliit.lk>
2025-09-16 16:42:17 +0530

    Add Lab07

---
+++ Lab Sheet 07/IT24102172.R
@@ -0,0 +1,12 @@
+setwd("C:\\Users\\MSI\\Desktop\\IT24102172")
+
+#Q1
+punif(25, min = 0, max = 40) - punif(10, min = 0, max = 40)
+
+#Q2
+pexp(2, rate = 1/3)
+
+#Q3
+1 - pnorm(130, mean = 100, sd = 15)
+
+qnorm(0.95, mean = 100, sd = 15)
